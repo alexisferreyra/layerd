f7a2d01a425c2686baeb8babcc35f5f80bcc3266
alexis.ferreyra@gmail.com <alexis.ferreyra@gmail.com@3dcb7c5b-6647-de3b-bec7-588bb40c6e51>
2012-10-19 01:22:47 +0000

    new python code generation module added brand new dont expect to work - based on javascript code generator
    pending:
    - almost everything
    - render namespaces as directories
    - one class per file 
    - dependencies between classes to generate imports
    
    working:
    namespace "py" used for accessing python libraries
    class "py.global" used to import python global symbols
    basic class and instance method generation
    basic statements may work and some expressions but doesn't have tests yet

---
+++ trunk/lib_zoec_outmod_python/CGFactory_PythonCodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2012 Intel Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alexis Ferreyra (Intel Corporation) - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LayerD.OutputModules
+{
+    public class CGFactory_PythonCodeGenerator
+    {
+        public CGFactory_PythonCodeGenerator()
+        {
+
+        }
+
+        public IEZOEOutputModuleServices GetServices()
+        {
+            return new Python_ZOE_Output_Module();
+        }
+    }
+}
